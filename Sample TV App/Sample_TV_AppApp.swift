0389269b3a0998f86e7ccf7015a85c7066408b64
Tyler Rister <tyler.rister@gmail.com>
2021-05-19 09:23:58 -0400

    Initial Commit

---
+++ Sample TV App/Sample_TV_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_TV_AppApp.swift
+//  Sample TV App
+//
+//  Created by Tyler Rister on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Sample_TV_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
